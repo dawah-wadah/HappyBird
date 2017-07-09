250da6b4a6f1d3c8d806b03322cb0b2d8816c2bf
Wadah Adlan <wadahadlan@gmail.com>
2017-07-09 19:49:54 -0400

    collisions tweaks

--- happy_bird.js
+++ happy_bird.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 5);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -68,7 +68,230 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__bird_Animations_js__ = __webpack_require__(6);
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__game_js__ = __webpack_require__(1);
+
+
+window.onload = function() {
+  const canvas = document.getElementById('canvas');
+  canvas.width = 2000;
+  canvas.height = 617;
+  const frames = 0;
+  const ctx = canvas.getContext('2d');
+  let gamePaused = false;
+  const game = new __WEBPACK_IMPORTED_MODULE_0__game_js__["a" /* default */](canvas, ctx, frames);
+
+
+
+
+  game.gameLoop();
+
+
+
+  window.addEventListener('keypress', (e) => {
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+      case 32:
+        game.bird.jump();
+        break;
+      case 112:
+      game.pauseGame();
+        break;
+      default:
+        console.log('this is not the key you are looking for');
+    }
+  });
+};
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__bird_js__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__foreground_js__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__tree_js__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__background_js__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pipe_js__ = __webpack_require__(7);
+
+
+
+
+
+
+class Game {
+  constructor(canvas, ctx, frames) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.frames = frames;
+    this.state = false;
+    this.background = new __WEBPACK_IMPORTED_MODULE_3__background_js__["a" /* default */](this.canvas, this.ctx);
+    this.foreground = new __WEBPACK_IMPORTED_MODULE_1__foreground_js__["a" /* default */](this.canvas, this.ctx);
+    this.bird = new __WEBPACK_IMPORTED_MODULE_0__bird_js__["a" /* default */](canvas, ctx, 200, 100, 100);
+    this.pipes = [];
+    this.trees = [];
+    this.gameID = 0;
+
+
+    this._generateRandomPipes = this._generateRandomPipes.bind(this);
+    this._getRandomIntInclusive = this._getRandomIntInclusive.bind(this);
+    this.checkArrays = this.checkArrays.bind(this);
+    this.assetsMaker = this.assetsMaker.bind(this);
+    this.gameLoop = this.gameLoop.bind(this);
+    this.isRunning = this.isRunning.bind(this);
+    this.checkUpperCollisions = this.checkUpperCollisions.bind(this);
+    this.checkLowerCollisions = this.checkLowerCollisions.bind(this);
+  }
+
+  isRunning(){
+    let game;
+    if (!this.state){
+      return (game = window.requestAnimationFrame(this.gameLoop));
+    } else {
+     return (window.cancelAnimationFrame(game));
+    }
+
+  }
+
+  pauseGame() {
+    if (!this.state) {
+      this.state = true;
+      console.log('paused');
+
+    } else {
+      console.log('unpaused');
+      this.gameID = window.requestAnimationFrame(this.gameLoop);
+      this.state = false;
+
+    }
+  }
+
+
+  _generateRandomPipes(context, canvasObject) {
+    let lengthTop = Math.floor(Math.random() * 200 + 100);
+    let lengthBottom = canvasObject.height - 150 - lengthTop;
+    let returnVal = {};
+    let speed = 7;
+    returnVal.top = new __WEBPACK_IMPORTED_MODULE_4__pipe_js__["a" /* default */](canvasObject.width,
+      0,
+      lengthTop,
+      speed,
+      context,
+      true);
+    returnVal.bottom = new __WEBPACK_IMPORTED_MODULE_4__pipe_js__["a" /* default */](canvasObject.width,
+      canvasObject.height - lengthBottom,
+      lengthBottom,
+      speed,
+      context,
+      false);
+    return returnVal;
+  }
+
+  assetsMaker() {
+    if (this.frames % 60 === 0) {
+      let pipeSet = this._generateRandomPipes(this.ctx, this.canvas);
+      this.pipes.push(pipeSet.top, pipeSet.bottom);
+      if (this._getRandomIntInclusive(20, 0) % 2 === 0) {
+        let shade = this._getRandomIntInclusive(0, 3);
+        this.trees.push(new __WEBPACK_IMPORTED_MODULE_2__tree_js__["a" /* default */](this.canvas, this.ctx, this.canvas.width, shade));
+      }
+    }
+
+    if (this.pipes.length > 10) {
+      this.pipes.shift();
+      this.pipes.shift();
+    }
+    if (this.trees.length > 10) {
+      this.trees.shift();
+      this.trees.shift();
+      this.trees.shift();
+    }
+  }
+
+  _getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  checkUpperCollisions(bird, pipe){
+    if (bird.yPos < pipe.length &&
+        bird.xPos + bird.width > pipe.xPos + pipe.width &&
+        bird.xPos < pipe.xPos + pipe.width){
+          console.log('collided with top');
+          this.pauseGame();
+
+        }
+  }
+  checkLowerCollisions(bird, pipe){
+    if (bird.yPos + bird.height >= pipe.yPos &&
+        bird.xPos + bird.width > pipe.xPos + pipe.width) {
+          // console.log('collided with bottom');
+
+        }
+  }
+
+
+  checkArrays() {
+
+    let fourPipes = this.pipes.slice(0, 5);
+    fourPipes.forEach((pipe) => {
+      if (pipe.top){
+        this.checkUpperCollisions(this.bird, pipe);
+
+      } else {
+        this.checkLowerCollisions(this.bird, pipe);
+      }
+      // if (this.bird.yPos + this.bird.height >= pipe.yPos &&
+      //   this.bird.yPos <= pipe.yPos + pipe.length &&
+      //   this.bird.xPos + this.bird.width >= pipe.xPos &&
+      //   this.bird.xPos <= pipe.xPos + pipe.width) {
+      //   console.log('shit collide');
+      // }
+    });
+  }
+
+  gameLoop() {
+    console.log(this.pipes.length);
+    console.log(this.trees.length);
+
+    this.frames++;
+    this.assetsMaker();
+    this.checkArrays();
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.background.update();
+    this.background.render();
+    this.trees.forEach((tree) => {
+      tree.update();
+      tree.render();
+    });
+    this.bird.update();
+    this.bird.render();
+    this.pipes.forEach((pipe) => {
+      pipe.update();
+      pipe.render();
+    });
+    this.foreground.update();
+    this.foreground.render();
+    if (!this.state){
+      this.gameID = window.requestAnimationFrame(this.gameLoop);
+    } else {
+      window.cancelAnimationFrame(this.gameID);
+    }
+  }
+
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Game);
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__bird_Animations_js__ = __webpack_require__(3);
 
 
 class Bird {
@@ -81,12 +304,13 @@ class Bird {
     this.height = height;
     this.yPos = canvas.height / 2;
     this.yVel = 0;
+    this.dead = false;
     this.termVelocity = 4;
     this.frames = 0;
-    this.rotation = 0;
     this.spritePicker = 0;
     this.image = new Image();
     this.image.src = 'res/flappybird2.png';
+    this.status =  this.status.bind(this);
   }
 
   update() {
@@ -138,196 +362,17 @@ class Bird {
     this.yVel = -9;
     this.frames += .1;
   }
+
+  status(){
+    return this.dead;
+  }
 }
 
 /* harmony default export */ __webpack_exports__["a"] = (Bird);
 
 
 /***/ }),
-/* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-class Foreground {
-  constructor(canvas, ctx) {
-    this.canvas = canvas;
-    this.ctx = ctx;
-    this.fgPos = 0;
-    this.fgSpeed = 4;
-    this.fgWidth = 336;
-    this.fgImg = new Image();
-    this.fgImg.src = 'res/ground.png';
-  }
-  
-  update() {
-    this.fgPos -= this.fgSpeed;
-    if (this.fgPos < -this.fgWidth) {
-      this.fgPos = 0;
-    }
-  }
-
-  render() {
-    for (var i = 0; i < this.canvas.width / this.fgWidth + 1; i++) {
-      this.ctx.drawImage(this.fgImg, this.fgPos + i * this.fgWidth, 505);
-    }
-  }
-}
-
-/* harmony default export */ __webpack_exports__["a"] = (Foreground);
-
-
-/***/ }),
-/* 2 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-class Tree {
-  constructor(canvas, ctx, xPos, spritePicker) {
-    this.image = new Image();
-    this.image.src = 'res/trees.png';
-    this.treesWidth = 76;
-    this.xPos = xPos;
-    this.spritePicker = spritePicker;
-    this.speed = 4;
-    this.canvas = canvas;
-    this.ctx = ctx;
-  }
-
-  update() {
-    this.xPos -= this.speed;
-  }
-
-  render() {
-    var spriteWidth = this.image.width / 4 * this.spritePicker;
-    this.ctx.drawImage(this.image, spriteWidth, 11,
-       this.image.width / 4, 100,
-       this.xPos,
-       505 - 90, 100, 100);
-  }
-}
-
-/* harmony default export */ __webpack_exports__["a"] = (Tree);
-
-
-/***/ }),
 /* 3 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-class Background {
-  constructor(canvas, ctx) {
-    this.canvas = canvas;
-    this.ctx = ctx;
-    this.bgPos = 0;
-    this.bgSpeed = 2;
-    this.bgWidth = 288;
-    this.bgImg = new Image();
-    this.bgImg.src = 'res/bg.png';
-  }
-
-
-
-update() {
-    this.bgPos -= this.bgSpeed;
-    if (this.bgPos < -this.bgWidth) {
-      this.bgPos = 0;
-    }
-  }
-
-render() {
-    for (var i = 0; i < this.canvas.width / this.bgWidth + 1; i++) {
-      this.ctx.drawImage(this.bgImg, this.bgPos + i * this.bgWidth, 0);
-    }
-  }
-}
-
-/* harmony default export */ __webpack_exports__["a"] = (Background);
-
-
-/***/ }),
-/* 4 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-class Pipe {
-  constructor(xPos, yPos, length, speed, ctx, top) {
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.length = length;
-    this.width = 100;
-    this.speed = speed;
-    this.ctx = ctx;
-    this.image = new Image();
-    // this.image.src = 'res/digletts.png';
-    this.image.src = 'res/59894.png';
-    this.top = top;
-  }
-update() {
-    this.xPos -= this.speed;
-  }
-render() {
-    if (this.top) {
-      // this.ctx.drawImage(this.image, 250,
-      this.ctx.drawImage(this.image, 56,
-        // 43, 114, 430, this.xPos, this.yPos, 100, this.length);
-        323, 26, 161, this.xPos, this.yPos, this.width, this.length);
-    } else {
-      // this.ctx.drawImage(this.image, 70,
-      this.ctx.drawImage(this.image, 85,
-        // 16, 114, 430, this.xPos, this.yPos, this.width, this.length);
-        323, 26, 161, this.xPos, this.yPos, this.width, this.length);
-    }
-  }
-}
-
-/* harmony default export */ __webpack_exports__["a"] = (Pipe);
-
-
-/***/ }),
-/* 5 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__game_js__ = __webpack_require__(7);
-
-
-window.onload = function() {
-  const canvas = document.getElementById('canvas');
-  canvas.width = 2000;
-  canvas.height = 617;
-  const frames = 0;
-  const ctx = canvas.getContext('2d');
-  let gamePaused = false;
-  const game = new __WEBPACK_IMPORTED_MODULE_0__game_js__["a" /* default */](canvas, ctx, frames);
-  // let newGame = setTimeout(game.gameLoop, 1000 / 30);
-
-
-
-
-// newGame();
-  game.gameLoop();
-
-
-
-  window.addEventListener('keypress', (e) => {
-    console.log(e.keyCode);
-    switch (e.keyCode) {
-      case 32:
-        game.bird.jump();
-        break;
-      case 112:
-      game.pauseGame();
-        break;
-      default:
-        console.log('this is not the key you are looking for');
-    }
-  });
-};
-
-
-/***/ }),
-/* 6 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -440,150 +485,139 @@ const birdAnimation = [
 
 
 /***/ }),
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Foreground {
+  constructor(canvas, ctx) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.fgPos = 0;
+    this.fgSpeed = 4;
+    this.fgWidth = 336;
+    this.fgImg = new Image();
+    this.fgImg.src = 'res/ground.png';
+  }
+  
+  update() {
+    this.fgPos -= this.fgSpeed;
+    if (this.fgPos < -this.fgWidth) {
+      this.fgPos = 0;
+    }
+  }
+
+  render() {
+    for (var i = 0; i < this.canvas.width / this.fgWidth + 1; i++) {
+      this.ctx.drawImage(this.fgImg, this.fgPos + i * this.fgWidth, 505);
+    }
+  }
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Foreground);
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Tree {
+  constructor(canvas, ctx, xPos, spritePicker) {
+    this.image = new Image();
+    this.image.src = 'res/trees.png';
+    this.treesWidth = 76;
+    this.xPos = xPos;
+    this.spritePicker = spritePicker;
+    this.speed = 4;
+    this.canvas = canvas;
+    this.ctx = ctx;
+  }
+
+  update() {
+    this.xPos -= this.speed;
+  }
+
+  render() {
+    var spriteWidth = this.image.width / 4 * this.spritePicker;
+    this.ctx.drawImage(this.image, spriteWidth, 11,
+       this.image.width / 4, 100,
+       this.xPos,
+       505 - 90, 100, 100);
+  }
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Tree);
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+class Background {
+  constructor(canvas, ctx) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.bgPos = 0;
+    this.bgSpeed = 2;
+    this.bgWidth = 288;
+    this.bgImg = new Image();
+    this.bgImg.src = 'res/bg.png';
+  }
+
+
+
+update() {
+    this.bgPos -= this.bgSpeed;
+    if (this.bgPos < -this.bgWidth) {
+      this.bgPos = 0;
+    }
+  }
+
+render() {
+    for (var i = 0; i < this.canvas.width / this.bgWidth + 1; i++) {
+      this.ctx.drawImage(this.bgImg, this.bgPos + i * this.bgWidth, 0);
+    }
+  }
+}
+
+/* harmony default export */ __webpack_exports__["a"] = (Background);
+
+
+/***/ }),
 /* 7 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__bird_js__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__foreground_js__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__tree_js__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__background_js__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pipe_js__ = __webpack_require__(4);
-
-
-
-
-
-
-class Game {
-  constructor(canvas, ctx, frames) {
-    this.canvas = canvas;
+class Pipe {
+  constructor(xPos, yPos, length, speed, ctx, top) {
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.length = length;
+    this.width = 100;
+    this.speed = speed;
     this.ctx = ctx;
-    this.frames = frames;
-    this.state = false;
-    this.background = new __WEBPACK_IMPORTED_MODULE_3__background_js__["a" /* default */](this.canvas, this.ctx);
-    this.foreground = new __WEBPACK_IMPORTED_MODULE_1__foreground_js__["a" /* default */](this.canvas, this.ctx);
-    this.bird = new __WEBPACK_IMPORTED_MODULE_0__bird_js__["a" /* default */](canvas, ctx, 200, 100, 100);
-    this.pipes = [];
-    this.trees = [];
-    this.gameID = 0;
-
-
-    this._generateRandomPipes = this._generateRandomPipes.bind(this);
-    this._getRandomIntInclusive = this._getRandomIntInclusive.bind(this);
-    this.checkArrays = this.checkArrays.bind(this);
-    this.assetsMaker = this.assetsMaker.bind(this);
-    this.gameLoop = this.gameLoop.bind(this);
-    this.isRunning = this.isRunning.bind(this);
+    this.image = new Image();
+    // this.image.src = 'res/digletts.png';
+    this.image.src = 'res/59894.png';
+    this.top = top;
   }
-
-  isRunning(){
-    let game;
-    if (!this.state){
-      return (game = window.requestAnimationFrame(this.gameLoop));
+update() {
+    this.xPos -= this.speed;
+  }
+render() {
+    if (this.top) {
+      this.ctx.drawImage(this.image, 56,
+        323, 26, 160, this.xPos, this.yPos, this.width, this.length);
     } else {
-     return (window.cancelAnimationFrame(game));
-    }
-
-  }
-
-  pauseGame() {
-    if (!this.state) {
-      this.state = true;
-      console.log('paused');
-
-    } else {
-      console.log('unpaused');
-      this.gameID = window.requestAnimationFrame(this.gameLoop);
-      this.state = false;
-
+      this.ctx.drawImage(this.image, 85,
+        323, 26, 160, this.xPos, this.yPos, this.width, this.length);
     }
   }
-
-
-  _generateRandomPipes(context, canvasObject) {
-    let lengthTop = Math.round(Math.random() * 200 + 100);
-    let lengthBottom = canvasObject.height - 120 - lengthTop;
-    let returnVal = {};
-    returnVal.top = new __WEBPACK_IMPORTED_MODULE_4__pipe_js__["a" /* default */](canvasObject.width, -5, lengthTop, 7, context, true);
-    returnVal.bottom = new __WEBPACK_IMPORTED_MODULE_4__pipe_js__["a" /* default */](canvasObject.width,
-      canvasObject.height + 10 - lengthBottom, lengthBottom, 7, context, false);
-    return returnVal;
-  }
-
-  assetsMaker() {
-    if (this.frames % 60 === 0) {
-      let pipeSet = this._generateRandomPipes(this.ctx, this.canvas);
-      this.pipes.push(pipeSet.top, pipeSet.bottom);
-      if (this._getRandomIntInclusive(20, 0) % 2 === 0) {
-        let shade = this._getRandomIntInclusive(0, 3);
-        this.trees.push(new __WEBPACK_IMPORTED_MODULE_2__tree_js__["a" /* default */](this.canvas, this.ctx, this.canvas.width, shade));
-      }
-    }
-
-    if (this.pipes.length > 10) {
-      this.pipes.shift();
-      this.pipes.shift();
-    }
-    if (this.trees.length > 10) {
-      this.trees.shift();
-      this.trees.shift();
-      this.trees.shift();
-    }
-  }
-
-  _getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-
-  checkArrays() {
-
-    let fourPipes = this.pipes.slice(0, 5);
-    fourPipes.forEach((pipe) => {
-      if (this.bird.yPos + this.bird.height >= pipe.yPos &&
-        this.bird.yPos <= pipe.yPos + pipe.length &&
-        this.bird.xPos + this.bird.width >= pipe.xPos &&
-        this.bird.xPos <= pipe.xPos + pipe.width) {
-        console.log('shit collide');
-      }
-    });
-  }
-
-  gameLoop() {
-    console.log(this.pipes.length);
-    console.log(this.trees.length);
-
-    this.frames++;
-    this.assetsMaker();
-    this.checkArrays();
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    this.background.update();
-    this.background.render();
-    this.trees.forEach((tree) => {
-      tree.update();
-      tree.render();
-    });
-    this.bird.update();
-    this.bird.render();
-    this.pipes.forEach((pipe) => {
-      pipe.update();
-      pipe.render();
-    });
-    this.foreground.update();
-    this.foreground.render();
-    if (!this.state){
-      this.gameID = window.requestAnimationFrame(this.gameLoop);
-    } else {
-      window.cancelAnimationFrame(this.gameID);
-    }
-  }
-
 }
 
-/* harmony default export */ __webpack_exports__["a"] = (Game);
+/* harmony default export */ __webpack_exports__["a"] = (Pipe);
 
 
 /***/ })
